5586a17902ab4d7919d9c42cd481a47f7a55e0ab
Sridhar <83635555+102642653@users.noreply.github.com>
2022-09-29 22:45:06 +1000

    Update login.dart
    Updated Login Design

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,8 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:workos_english/screens/auth/forget_pass.dart';
 import 'package:workos_english/screens/auth/register.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -10,8 +10,6 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _animation;
   late TextEditingController _emailTextController =
       TextEditingController(text: '');
   late TextEditingController _passTextController =
@@ -22,31 +20,12 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    _animationController.dispose();
     _emailTextController.dispose();
     _passTextController.dispose();
     _passFocusNode.dispose();
     super.dispose();
   }
 
-  @override
-  void initState() {
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 20));
-    _animation =
-        CurvedAnimation(parent: _animationController, curve: Curves.linear)
-          ..addListener(() {
-            setState(() {});
-          })
-          ..addStatusListener((animationStatus) {
-            if (animationStatus == AnimationStatus.completed) {
-              _animationController.reset();
-              _animationController.forward();
-            }
-          });
-    _animationController.forward();
-    super.initState();
-  }
 
   void _submitFormOnLogin() {
     final isValid = _loginFormKey.currentState!.validate();
@@ -55,23 +34,23 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       // backgroundColor: Colors.blue,
-      body: Stack(
-        children: [
-          CachedNetworkImage(
-            imageUrl:
-                "https://cdn.discordapp.com/attachments/832512849451548682/1021376517160308806/photoholgic-jK9dT34TfuI-unsplash.jpg",
-            placeholder: (context, url) => Image.asset(
-              'assets/images/wallpaper.jpg',
-              fit: BoxFit.fill,
+      body: SafeArea(
+        child: Stack(children: [
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Image(image: AssetImage('assets/images/shape1.png')),
             ),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-            alignment: FractionalOffset(_animation.value, 0),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image(image: AssetImage('assets/images/shape1.png')),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(
@@ -80,46 +59,19 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
             child: ListView(
               children: [
                 SizedBox(
-                  height: size.height * 0.1,
+                  height: 30,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SvgPicture.asset('assets/images/person.svg',
+                      height: 175, width: 200),
                 ),
                 Text(
                   'Login',
                   style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.black54,
                       fontWeight: FontWeight.bold,
                       fontSize: 30),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Don\'t have an account?',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
-                      ),
-                      TextSpan(text: '     '),
-                      TextSpan(
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => SignUp(),
-                                ),
-                              ),
-                        text: 'Regiter',
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Colors.blue.shade300,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
-                      )
-                    ],
-                  ),
                 ),
                 SizedBox(
                   height: 40,
@@ -142,15 +94,18 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
                             return null;
                           }
                         },
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.w500),
                         decoration: InputDecoration(
                           hintText: 'Email',
-                          hintStyle: TextStyle(color: Colors.white),
+                          hintStyle: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500),
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           errorBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.red),
@@ -173,8 +128,10 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
                             return null;
                           }
                         },
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.w500),
                         decoration: InputDecoration(
+
                           suffixIcon: GestureDetector(
                             onTap: () {
                               setState(() {
@@ -185,16 +142,18 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
                               _obscureText
                                   ? Icons.visibility
                                   : Icons.visibility_off,
-                              color: Colors.white,
+                              color: Colors.black54,
                             ),
                           ),
                           hintText: 'Password',
-                          hintStyle: TextStyle(color: Colors.white),
+                          hintStyle: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500),
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           errorBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.red),
@@ -205,7 +164,7 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
                   ),
                 ),
                 SizedBox(
-                  height: 15,
+                  height: 10,
                 ),
                 Align(
                   alignment: Alignment.bottomRight,
@@ -221,19 +180,20 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
                     child: Text(
                       'Forgot password?',
                       style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.black54,
                           fontSize: 17,
                           decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.w500,
                           fontStyle: FontStyle.italic),
                     ),
                   ),
                 ),
                 SizedBox(
-                  height: 40,
+                  height: 10,
                 ),
                 MaterialButton(
                   onPressed: _submitFormOnLogin,
-                  color: Colors.pink.shade700,
+                  color: Colors.blue.shade700,
                   elevation: 8,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(13)),
@@ -259,11 +219,58 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
                           ),
                         ]),
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Don\'t have an account?',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16),
+                        ),
+                        TextSpan(text: '     '),
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SignUp(),
+                              ),
+                            ),
+                          text: 'Register',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.pink.shade700,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    height: 80,
+                    width: 100,
+                  ),
+                ),
               ],
             ),
           ),
-        ],
+        ]
+        ),
       ),
     );
   }
