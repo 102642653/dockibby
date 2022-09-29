ca86d4cd7f059c4370d7cd4e6bf29addce34e4e9
Sridhar <83635555+102642653@users.noreply.github.com>
2022-09-29 22:43:03 +1000

    Update register.dart
    Updatd Register Design

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,6 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:workos_english/screens/auth/login.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -8,57 +9,36 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _animation;
   late TextEditingController _fullNameController =
       TextEditingController(text: '');
   late TextEditingController _emailTextController =
       TextEditingController(text: '');
   late TextEditingController _passTextController =
       TextEditingController(text: '');
-  late TextEditingController _DVAfileNumberController =
+  late TextEditingController _phoneNumberController =
       TextEditingController(text: '');
 
   FocusNode _emailFocusNode = FocusNode();
 
   FocusNode _passFocusNode = FocusNode();
 
-  FocusNode _DVAFocusNode = FocusNode();
+  FocusNode _phoneFocusNode = FocusNode();
   bool _obscureText = true;
   final _signUpFormKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
-    _animationController.dispose();
     _fullNameController.dispose();
     _emailTextController.dispose();
     _passTextController.dispose();
-    _DVAfileNumberController.dispose();
+    _phoneNumberController.dispose();
     _emailFocusNode.dispose();
     _passFocusNode.dispose();
-    _DVAFocusNode.dispose();
+    _phoneFocusNode.dispose();
 
     super.dispose();
   }
 
-  @override
-  void initState() {
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 20));
-    _animation =
-        CurvedAnimation(parent: _animationController, curve: Curves.linear)
-          ..addListener(() {
-            setState(() {});
-          })
-          ..addStatusListener((animationStatus) {
-            if (animationStatus == AnimationStatus.completed) {
-              _animationController.reset();
-              _animationController.forward();
-            }
-          });
-    _animationController.forward();
-    super.initState();
-  }
 
   void _submitFormOnSignUp() {
     final isValid = _signUpFormKey.currentState!.validate();
@@ -67,23 +47,23 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       // backgroundColor: Colors.blue,
       body: Stack(
         children: [
-          CachedNetworkImage(
-            imageUrl:
-                "https://cdn.discordapp.com/attachments/832512849451548682/1021376517160308806/photoholgic-jK9dT34TfuI-unsplash.jpg",
-            placeholder: (context, url) => Image.asset(
-              'assets/images/wallpaper.jpg',
-              fit: BoxFit.fill,
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Image(image: AssetImage('assets/images/shape1.png')),
             ),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-            alignment: FractionalOffset(_animation.value, 0),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image(image: AssetImage('assets/images/shape1.png')),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(
@@ -92,46 +72,22 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
             child: ListView(
               children: [
                 SizedBox(
-                  height: size.height * 0.1,
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SvgPicture.asset('assets/images/person.svg',
+                      height: 175, width: 200),
                 ),
                 Text(
                   'SignUp',
                   style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.black54,
                       fontWeight: FontWeight.bold,
                       fontSize: 30),
                 ),
                 SizedBox(
                   height: 10,
-                ),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Already have an account?',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
-                      ),
-                      TextSpan(text: '     '),
-                      TextSpan(
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => Navigator.canPop(context)
-                              ? Navigator.pop(context)
-                              : null,
-                        text: 'Login',
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Colors.blue.shade300,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
                 ),
                 Form(
                   key: _signUpFormKey,
@@ -151,15 +107,18 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                             return null;
                           }
                         },
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.w500),
                         decoration: InputDecoration(
                           hintText: 'Full Name',
-                          hintStyle: TextStyle(color: Colors.white),
+                          hintStyle: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500),
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           errorBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.red),
@@ -167,7 +126,7 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                         ),
                       ),
                       SizedBox(
-                        height: 20,
+                        height: 10,
                       ),
                       //Email
                       TextFormField(
@@ -184,15 +143,18 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                             return null;
                           }
                         },
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.w500),
                         decoration: InputDecoration(
                           hintText: 'Email',
-                          hintStyle: TextStyle(color: Colors.white),
+                          hintStyle: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500),
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           errorBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.red),
@@ -200,13 +162,13 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                         ),
                       ),
                       SizedBox(
-                        height: 20,
+                        height: 10,
                       ),
                       //Password
                       TextFormField(
                         textInputAction: TextInputAction.next,
-                        onEditingComplete: () =>
-                            FocusScope.of(context).requestFocus(_DVAFocusNode),
+                        onEditingComplete: () => FocusScope.of(context)
+                            .requestFocus(_phoneFocusNode),
                         focusNode: _passFocusNode,
                         obscureText: _obscureText,
                         keyboardType: TextInputType.visiblePassword,
@@ -230,16 +192,18 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                               _obscureText
                                   ? Icons.visibility
                                   : Icons.visibility_off,
-                              color: Colors.white,
+                              color: Colors.black54,
                             ),
                           ),
                           hintText: 'Password',
-                          hintStyle: TextStyle(color: Colors.white),
+                          hintStyle: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500),
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
+                            borderSide: BorderSide(color: Colors.black45),
                           ),
                           errorBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.red),
@@ -247,44 +211,47 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                         ),
                       ),
                       SizedBox(
-                        height: 20,
+                        height: 10,
                       ),
-                      // //DVANumber
-                      // TextFormField(
-                      //   textInputAction: TextInputAction.done,
-                      //   onEditingComplete: () => _submitFormOnSignUp,
-                      //   focusNode: _DVAFocusNode,
-                      //   keyboardType: TextInputType.text,
-                      //   controller: _DVAfileNumberController,
-                      //   validator: (value) {
-                      //     if (value!.isEmpty ||
-                      //         value.length < 8 ||
-                      //         value.length > 9) {
-                      //       return "Please enter a valid DVA file number";
-                      //     } else {
-                      //       return null;
-                      //     }
-                      //   },
-                      //   style: TextStyle(color: Colors.white),
-                      //   decoration: InputDecoration(
-                      //     hintText: 'DVA File Number',
-                      //     hintStyle: TextStyle(color: Colors.white),
-                      //     enabledBorder: UnderlineInputBorder(
-                      //       borderSide: BorderSide(color: Colors.white),
-                      //     ),
-                      //     focusedBorder: UnderlineInputBorder(
-                      //       borderSide: BorderSide(color: Colors.white),
-                      //     ),
-                      //     errorBorder: UnderlineInputBorder(
-                      //       borderSide: BorderSide(color: Colors.red),
-                      //     ),
-                      //   ),
-                      // ),
+                      //Phone number
+                      TextFormField(
+                        textInputAction: TextInputAction.done,
+                        onEditingComplete: () => _submitFormOnSignUp,
+                        focusNode: _phoneFocusNode,
+                        keyboardType: TextInputType.phone,
+                        controller: _phoneNumberController,
+                        validator: (value) {
+                          if (value!.isEmpty ||
+                              value.length < 10 ||
+                              value.length > 10) {
+                            return "Please enter a valid phone number";
+                          } else {
+                            return null;
+                          }
+                        },
+                        style: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.w500),
+                        decoration: InputDecoration(
+                          hintText: 'Enter Phone number',
+                          hintStyle: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black45),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black45),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
                 SizedBox(
-                  height: 80,
+                  height: 40,
                 ),
                 MaterialButton(
                   onPressed: _submitFormOnSignUp,
@@ -314,7 +281,53 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                           ),
                         ]),
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Already have an account?',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16),
+                        ),
+                        TextSpan(text: '     '),
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Login(),
+                              ),
+                            ),
+                          text: 'Login',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.blue.shade700,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    height: 80,
+                    width: 100,
+                  ),
+                ),
               ],
             ),
           ),
